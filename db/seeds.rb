b870e150e2696bdfe9a180e4f58acffaaadb87b9
Charles Sumner <cws1717@gmail.com>
2018-08-10 10:50:55 +0200

    Prepare for Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+word_serialized = URI.parse(url).read
+word = JSON.parse(word_serialized)
+
+ingredients = word['drinks']
+
+ingredients.each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
+
+Cocktail.create(name: "Old Fashioned")
+Cocktail.create(name: "Manhattan")
+Cocktail.create(name: "White Russian")
+Cocktail.create(name: "Pisco Sour")
+Cocktail.create(name: "Cosmopolitan")
+Cocktail.create(name: "Aviator")
+Cocktail.create(name: "Whisky Sour")
+Cocktail.create(name: "Tom Collins")
+Cocktail.create(name: "Irish Coffee")
+Cocktail.create(name: "Bloody Mary")
+Cocktail.create(name: "Mint Julep")
+Cocktail.create(name: "White Sangria")
